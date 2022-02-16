e82ba3bd1e2fc1f382cd94569c3d59f502ba2675
Keyoshi <81902787+Keyoshi083@users.noreply.github.com>
2022-02-16 18:27:33 +0900

    コンポーネント基礎学習 (#2)
    * .vueファイルとは
    
    * コンポーネントをグローバル登録する

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from "vue";
+//.vueファイルはインポートすると、componentオブジェクトとなる
 import App from "./App.vue";
-
+import LikeNumber from "./LikeNumber.vue";
 //trueにすると、ブラウザコンソール上に補助ログが出る。
 Vue.config.productionTip = false;
 
+//コンポーネントをグローバル登録
+Vue.component("LikeNumber", LikeNumber);
 new Vue({
   render: (h) => h(App),
 }).$mount("#app");
