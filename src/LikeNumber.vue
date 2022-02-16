717c83d1a8f7366f1bd5e31d73aad6971544e2ce
Keyoshi083 <gt.chicken1@gmail.com>
2022-02-16 18:24:26 +0900

    コンポーネントをグローバル登録する

---
+++ src/LikeNumber.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>いいね {{ number }}</p>
+    <button @click="increment">増やす</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LikeNumber",
+  data() {
+    return {
+      number: 5,
+    };
+  },
+  methods: {
+    increment() {
+      this.number += 1;
+    },
+  },
+};
+</script>
